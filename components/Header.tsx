0a01d9b9897f06a4a5412920d9731fb222bcaf15
marouane270 <boumadafa22@gmail.com>
2022-09-22 12:37:42 +0200

    neew

--- components/Header.tsx
+++ components/Header.tsx
@@ -46,7 +46,7 @@ const Header: React.FC = () => {
       <div className="left">
         <Link href="/">
           <a className="bold" data-active={isActive('/')}>
-            Fil d&apos;actualité
+            Posts
           </a>
         </Link>
         <style jsx>{`
@@ -118,7 +118,7 @@ const Header: React.FC = () => {
       <div className="left">
         <Link href="/">
           <a className="bold" data-active={isActive('/')}>
-            Fil d&apos;actualité
+            Posts
           </a>
         </Link>
         <Link href="/drafts">
--- pages/index.tsx
+++ pages/index.tsx
@@ -32,7 +32,7 @@ const Blog: React.FC<Props> = (props) => {
   return (
     <Layout>
       <div className="page">
-        <h1>Fil d&apos;actualité </h1>
+        <h1>Posts </h1>
         <main>
           {props.feed.map((post) => (
             <div key={post.id} className="post">
