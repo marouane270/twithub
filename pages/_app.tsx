ecd12c2af838b8c3ba4e9f9bc0d555969211581f
marouane270 <boumadafa22@gmail.com>
2022-09-22 13:27:16 +0200

    new commiit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 
 import { SessionProvider } from 'next-auth/react';
+import { Session } from "next-auth";
 import { AppProps } from 'next/app';
 
-const App = ({ Component, pageProps }: AppProps) => {
+
+function App({
+  Component,
+  pageProps,
+}: AppProps<{
+  session: Session;
+}>) {
+
+//const App = ({ Component, pageProps }: AppProps) => {
   return (
 
     <SessionProvider session={pageProps.session}>
